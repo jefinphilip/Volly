47dee7704e100ccaa88e0123236592042e3755f6
jefin <jeffinphilip@gmail.com>
2016-08-26 17:34:50 +0530

    just changed

--- app/src/main/java/com/example/clement/volly/MainActivity.java
+++ app/src/main/java/com/example/clement/volly/MainActivity.java
@@ -11,16 +11,22 @@ import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.android.volley.Cache;
+import com.android.volley.Network;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
 
      Button button;
+    Button button1;
      TextView textView;
      String msgFrmServer = "http://192.168.1.3/greety.php";
 
@@ -42,13 +48,20 @@ public class MainActivity extends AppCompatActivity {
 
 
         button= (Button)findViewById(R.id.dwnld);
+        button1= (Button)findViewById(R.id.dwnld1);
         textView=(TextView)findViewById(R.id.txt);
+
+        //custom request Queue
+        Cache cache= new DiskBasedCache(getCacheDir(),1024*1024);
+        Network network= new BasicNetwork(new HurlStack());
+        final RequestQueue requestQueue= new RequestQueue(cache,network);
+        requestQueue.start();
 
          button.setOnClickListener(new View.OnClickListener() {
              @Override
              public void onClick(View v) {
 
-                 final RequestQueue requestQueue= Volley.newRequestQueue(MainActivity.this);
+
 
                  StringRequest stringRequest= new StringRequest(Request.Method.POST, msgFrmServer,
                          new Response.Listener<String>() {
@@ -72,6 +85,34 @@ public class MainActivity extends AppCompatActivity {
                             requestQueue.add(stringRequest);
              }
          });
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final RequestQueue requestQueue= Volley.newRequestQueue(MainActivity.this);
+
+                StringRequest stringRequest= new StringRequest(Request.Method.POST, msgFrmServer,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+
+                                textView.setText(response);
+                                requestQueue.stop();
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                        textView.setText("some thing went wrong");
+                        error.printStackTrace();
+                        requestQueue.stop();
+
+                    }
+                });
+
+                requestQueue.add(stringRequest);
+            }
+        });
     }
 
     @Override
